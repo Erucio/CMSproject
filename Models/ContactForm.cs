ca0810a0cf4f62e93e1f23a4f83db0fbe6a1308e
Erucio <111879095+Erucio@users.noreply.github.com>
2023-10-04 11:29:18 +0200

    trying to commit

---
+++ Models/ContactForm.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CMSProject.Models
+{
+    public class ContactForm
+    {
+        [Required(ErrorMessage = "Please enter your name.")]
+        [StringLength(50, ErrorMessage = "Name should be no more than 50 characters.")]
+        public string Name { get; set; } = null!;
+
+        [Required(ErrorMessage = "Please enter your email address.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        public string Email { get; set; } = null!;
+
+        [Required(ErrorMessage = "Please enter your message.")]
+        public string Message { get; set; } = null!;
+        public string? RedirectUrl { get; set; } = "/contact";
+    }
+}
